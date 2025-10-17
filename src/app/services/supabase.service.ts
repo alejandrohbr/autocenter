d01baec1605e399cfb1fdc86eccb24736aedb0da
alejandrohbr <hugoromob@gmail.com>
2025-10-17 12:09:32 -0600

    Added vite-env.d.ts

--- src/app/services/supabase.service.ts
+++ src/app/services/supabase.service.ts
@@ -8,13 +8,17 @@ export class SupabaseService {
   private supabase: SupabaseClient;
 
   constructor() {
-    const supabaseUrl = 'https://ldomgkpkpcgiuojszqur.supabase.co';
-    const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imxkb21na3BrcGNnaXVvanN6cXVyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTk0Nzc0NjMsImV4cCI6MjA3NTA1MzQ2M30.9lESj-Keb_I2-Dhah2OUYodbU-HhDi3PGbkBRUZOd2Y';
+    const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+    const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+
+    if (!supabaseUrl || !supabaseKey) {
+      throw new Error('Missing Supabase environment variables');
+    }
 
     this.supabase = createClient(supabaseUrl, supabaseKey, {
       auth: {
-        persistSession: false,
-        autoRefreshToken: false,
+        persistSession: true,
+        autoRefreshToken: true,
         detectSessionInUrl: false
       }
     });
